2ad82cc8f54df5c0302252ba17129d0279f52435
wufudong <wfd19940918@163.com>
2022-07-05 14:02:54 +0800

    test

--- project06/V3版本鉴权说明/demo_时间戳的生产.py
+++ project06/V3版本鉴权说明/demo_时间戳的生产.py
@@ -8,5 +8,6 @@ print('当前的时间戳：', t)
 # 签名的获取
 # token的前50位 + 时间戳，然后进行RSA加密
 
-token = 'asdasfsafsfdgdfhfggsdfsysdfsdfsdasdf.asdasfgadsag.asdafasdafadasfasdgasdfasdasdasd'
+token = 'asdasfsafsfdgdfhfggsdfsysdfsdfsdasdf.asdasfgkadsag.asdafasdafadasfasdgasdfasdasdasd'
 data = token[:50] + str(t)
+print(data)
